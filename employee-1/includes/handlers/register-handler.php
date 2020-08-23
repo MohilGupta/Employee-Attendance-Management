9b25243aacbb2cc2662cd3e72ecbff2934cb4812
Mohil Gupta <mohil3289@gmail.com>
2020-08-23 19:02:46 +0530

    Updated PHP mailer and port number

--- employee-1/includes/handlers/register-handler.php
+++ employee-1/includes/handlers/register-handler.php
@@ -5,6 +5,10 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 function register($org,$add,$email,$mobile,$city,$state,$password,$token,$con){
     $encryptedPassword=md5($password);
+    $email_exists=mysqli_query($con,"SELECT * FROM organisations WHERE email='$email'");
+    if(mysqli_num_rows($email_exists)>0){
+      return false;
+    }
     $result=mysqli_query($con,"INSERT INTO organisations VALUES ('','$org','$email','$mobile','$add','$city','$state','$encryptedPassword','$token',0)");
     return $result;
 
@@ -20,12 +24,26 @@ if(isset($_POST['registerButton'])){
     $city=$_POST['city'];
     $state=$_POST['state'];
     $password=$_POST['password'];
-    
+    //$password=$_POST['repassword'];
     $token="qwerfdsSDFWEFVRVCrwrefuvbev124t35t5eev0024r248r35ervb9230r24v";
     $token=str_shuffle($token);
     $token=substr($token,0,10);
 
     $successfulRegistration=register($organisation,$address,$email,$mobile,$city,$state,$password,$token,$con);
+    if($successfulRegistration==false){
+        echo "
+        
+        <script>
+        
+        alert('Email already exists');
+        
+        location.assign('index.php');
+        
+        </script>
+        
+        ";
+       // header("Location:index.php");
+    }
     if($successfulRegistration){
         echo "<script>
         
@@ -41,21 +59,16 @@ if(isset($_POST['registerButton'])){
         $subject="email verfication";
         $mail=new PHPMailer();
         $mail->isSMTP();
-
-        $mail->Host="mail.innerworkindia.com";
+        $mail->Host="smtp.gmail.com";
         $mail->SMTPAuth=true;
-
-        $mail->Username="response@innerworkindia.com";   
-        $mail->Password="123@Response";   
-  
-        $mail->Port=587;
-  
-        $mail->SMTPSecure="tls";
+        $mail->Username="beingtherebuisness@gmail.com";   
+        $mail->Password="beingthere#123";                
+        $mail->Port=465;
+        $mail->SMTPSecure="ssl";
     
         // email setings
         $mail->isHTML(true);
-       // $mail->setFrom("beingtherebuisness@gmail.com");   // Please change the email here 
-       $mail->setFrom("response@innerworkindia.com"); 
+        $mail->setFrom("beingtherebuisness@gmail.com");   
         $mail->addAddress($email);
         $mail->Subject=$subject;
         $mail->Body=  "
